a90b87e9e584f71650954dced4e1d27a9176380c
xaherpoz <58935164+xaherpoz@users.noreply.github.com>
2021-06-13 10:04:41 +0200

    Programa Java completo

---
+++ GithubJava/src/App.java
@@ -0,0 +1,16 @@
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+
+        cod a = new cod();
+
+        String s1 = a.leerTxt("D:\\1GRADO_SUPERIOR_DAW\\DESPLIEGUE\\GitHub Java\\GithubJava\\src\\datos.txt");
+
+       System.out.println(s1);
+   
+
+       
+    }
+}
